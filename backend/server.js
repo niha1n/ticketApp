b76833b218a47c9c5a7aac78bec3eec618c74f10
Nihal N <81716192+niha1n@users.noreply.github.com>
2023-04-12 02:59:43 +0530

    prepare for deploy 7

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require("express");
 const colors = require("colors");
-
+const cors = require('cors');
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 const { errorHandler } = require("./middleware/errorMiddleware");
